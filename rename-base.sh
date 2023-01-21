69fed2c18039de0aaf0d786cb21e125fd29b3a9e
Leif <zmeriksen@icloud.com>
2023-01-21 13:32:31 -0700

    Update rename-base sh file

--- rename-base.sh
+++ rename-base.sh
@@ -2,4 +2,10 @@
 
 read -p 'Project Name: ' projectvar
 
-grep -rli 'base' * | xargs -I@ sed -i '' 's/base/$projectvar/g' @
+grep -rli 'base' * | xargs -I@ sed -i '' "s/base/$projectvar/g" @
+
+for a in *;
+do if [ -d "$a" ]; then
+ mv -- "$a" "$projectvar"
+fi
+done
