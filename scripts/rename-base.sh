623f6583d534e98280ffddb3ce1dcb4ea0aceb7f
Leif <zmeriksen@icloud.com>
2023-01-21 15:55:02 -0700

    Update GitHub workflow files

--- scripts/rename-base.sh
+++ scripts/rename-base.sh
@@ -3,3 +3,7 @@
 find . -depth -name '*base*' -execdir bash -c 'mv -i "$1" "${1//base/NEW_NAME}"' bash {} \;
 
 grep -rli 'base' * | xargs -I@ sed -i '' "s/base/NEW_NAME/g" @
+
+cd ../.github/workflows/
+
+grep -rli 'base' * | xargs -I@ sed -i '' "s/base/NEW_NAME/g" @
