eb380084fa1cb5aa5a8408fa94cef39a8906a88f
Leif <zmeriksen@icloud.com>
2023-01-21 15:13:29 -0700

    Add cleanup and xcodegen

--- rename.sh
+++ rename.sh
@@ -5,3 +5,8 @@ read -p 'Project Name: ' projectvar
 grep -rli 'NEW_NAME' scripts/rename-base.sh | xargs -I@ sed -i '' "s/NEW_NAME/$projectvar/g" @
 
 sh ./scripts/rename-base.sh
+
+xcodegen generate
+
+rm -rf scripts
+rm rename.sh
