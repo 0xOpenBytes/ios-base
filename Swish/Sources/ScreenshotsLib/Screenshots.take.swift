ebd9763922ce1bf20d2f0fd8caccc93d36330edc
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2023-01-22 20:26:51 -0500

    typo

--- Swish/Sources/ScreenshotsLib/Screenshots.take.swift
+++ Swish/Sources/ScreenshotsLib/Screenshots.take.swift
@@ -5,25 +5,25 @@ extension Screenshots {
   func take() throws {
     try FileManager.default.createDirectory(atPath: derivedDataPath, withIntermediateDirectories: true)
     try FileManager.default.createDirectory(atPath: logsPath, withIntermediateDirectories: true)
-    
+
     try Device.allCases.forEach { device in
       try sh(.file(logsPath + "/\(device.name).build.log"), """
-        xcodebuild -project Base.xcodeproj -scheme Screenshots -destination 'platform=iOS Simulator,name=\(device.simulatorName)' -derivedDataPath "\(derivedDataPath)" test
+        xcodebuild -project base.xcodeproj -scheme Screenshots -destination 'platform=iOS Simulator,name=\(device.simulatorName)' -derivedDataPath "\(derivedDataPath)" test
         """)
-            
+
       let xcresultPath = try findLatestXCResult(in: derivedDataPath)
       try FileManager.default.createDirectory(atPath: "\(pngsPath)/\(device.name)", withIntermediateDirectories: true)
       try sh(.terminal, "xcparse screenshots \(xcresultPath) \(pngsPath)/\(device.name)")
     }
   }
-  
+
   private func findLatestXCResult(in derivedDataPath: String) throws -> String {
     let testLogsPath = derivedDataPath + "/Logs/Test"
     let files = try FileManager.default.contentsOfDirectory(atPath: testLogsPath)
     let latest = files
       .filter({ $0.hasSuffix("xcresult")})
       .max()
-    
+
     if let latest = latest {
       return testLogsPath + "/" + latest
     } else {
@@ -33,5 +33,5 @@ extension Screenshots {
       throw XCResultError.latestNotFound
     }
   }
-  
+
 }
