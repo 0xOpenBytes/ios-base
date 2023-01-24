ce84623b33aa5e5c0f1e09f47130f1375173c87b
Leif <zmeriksen@icloud.com>
2023-01-24 16:38:09 -0700

    Rename base in .swiftlint.yml

--- scripts/rename-base.sh
+++ scripts/rename-base.sh
@@ -3,6 +3,7 @@
 find . -depth -name '*base*' -execdir bash -c 'mv -i "$1" "${1//base/NEW_NAME}"' bash {} \;
 
 grep -rli 'base' * | xargs -I@ sed -i '' "s/base/NEW_NAME/g" @
+grep -rli 'base' .swiftlint.yml | xargs -I@ sed -i '' "s/base/NEW_NAME/g" @
 
 cd .github/workflows/
 
