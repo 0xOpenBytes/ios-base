0c539909e8fce7eb402ed55abd180a19a1c17b6f
Leif <zmeriksen@icloud.com>
2023-03-05 22:15:46 -0700

    Update changes from c at 4.0.0

--- base/Persistence/Caching/Data/DataCache.swift
+++ base/Persistence/Caching/Data/DataCache.swift
@@ -18,53 +18,6 @@ import c
 import Foundation
 import SwiftUI
 
-class DataCache: ObservableObject {
-    private var lock: NSLock = NSLock()
-    private var cache: c.KeyedCache<String> = .init()
-
+class DataCache: c.Cache<AnyHashable, DataCaching>, ObservableObject {
     static var shared: DataCache = DataCache()
-
-    var valuesInCache: [String: DataCaching] {
-        cache.valuesInCache(ofType: DataCaching.self)
-    }
-
-    private init() { }
-
-    func get<Value: DataCaching>(_ key: String, type: Value.Type = Value.self) -> Value? {
-        cache.get(key, as: Value.self)
-    }
-
-    func resolve<Value: DataCaching>(_ key: String, type: Value.Type = Value.self) throws -> Value {
-        try cache.resolve(key, as: Value.self)
-    }
-
-    func set(value: DataCaching, forKey key: String) {
-        lock.lock()
-        cache.set(value: value, forKey: key)
-        lock.unlock()
-        objectWillChange.send()
-    }
-
-    func remove(_ key: String) {
-        lock.lock()
-        cache.remove(key)
-        lock.unlock()
-        objectWillChange.send()
-    }
-
-    func contains(_ key: String) -> Bool {
-        cache.contains(key)
-    }
-
-    func require(keys: Set<String>) throws -> Self {
-        _ = try cache.require(keys: keys)
-
-        return self
-    }
-
-    func require(_ key: String) throws -> Self {
-        _ = try cache.require(key)
-
-        return self
-    }
 }
--- base/Persistence/Caching/Image/ImageCache.swift
+++ base/Persistence/Caching/Image/ImageCache.swift
@@ -18,53 +18,10 @@ import c
 import Foundation
 import SwiftUI
 
-class ImageCache: ObservableObject {
-    private var lock: NSLock = NSLock()
-    private var cache: c.KeyedCache<URL> = .init()
-
+class ImageCache: c.Cache<AnyHashable, Image>, ObservableObject {
     static var shared: ImageCache = ImageCache()
 
-    var valuesInCache: [URL: Image] {
-        cache.valuesInCache(ofType: Image.self)
-    }
-
-    private init() { }
-
-    func get(_ key: URL) -> Image? {
-        cache.get(key, as: Image.self)
-    }
-
-    func resolve(_ key: URL) throws -> Image {
-        try cache.resolve(key, as: Image.self)
-    }
-
-    func set(image: Image, forKey key: URL) {
-        lock.lock()
-        cache.set(value: image, forKey: key)
-        lock.unlock()
-        objectWillChange.send()
-    }
-
-    func remove(_ key: URL) {
-        lock.lock()
-        cache.remove(key)
-        lock.unlock()
-        objectWillChange.send()
-    }
-
-    func contains(_ key: URL) -> Bool {
-        cache.contains(key)
-    }
-
-    func require(keys: Set<URL>) throws -> Self {
-        _ = try cache.require(keys: keys)
-
-        return self
-    }
-
-    func require(_ key: URL) throws -> Self {
-        _ = try cache.require(key)
-
-        return self
+    func set(image: Image, forKey key: AnyHashable) {
+        set(value: image, forKey: key)
     }
 }
